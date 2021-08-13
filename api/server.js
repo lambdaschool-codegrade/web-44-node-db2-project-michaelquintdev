b2027f7bdcce4d94df479250e756b3eb2d549841
michaelquintdev <michaelquintdev@gmail.com>
2021-08-13 16:49:34 -0500

    knex added

--- api/server.js
+++ api/server.js
@@ -3,5 +3,10 @@ const express = require("express")
 const server = express()
 
 // DO YOUR MAGIC
+server.use(express.json())
+
+server.use('*', (req, res, next) => {
+    next({status: 404, message: 'not found'})
+})
 
 module.exports = server
---
+++ data/seeds/01-cars.js
@@ -0,0 +1,32 @@
+// const { default: knex } = require("knex")
+
+const cars = [
+    {
+        vin: '1234567891012',
+        make: 'Acura',
+        model: 'TLX',
+        mileage: 0,
+        title: 'noice',
+        transmission: 'automatic'
+    },
+    {
+        vin: '1234567891012',
+        make: 'Genesis',
+        model: '370',
+        mileage: 0,
+        title: 'new',
+        transmission: 'automatic'
+    },
+]
+
+// exports.seed = function(knex) {
+//     return knex('cars')
+//     .truncate().then(() => {
+//         return knex('cars').insert(cars)
+//     })
+// }
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
